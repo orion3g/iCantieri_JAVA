5ce5c6ae79b218a570f7c025a821b96ee3c06118
MELANY TREMANTE <melanytremante@gmail.com>
2021-11-26 15:36:44 +0100

    ciao

---
+++ iCantieri2.0/src/classi/Area.java
@@ -0,0 +1,38 @@
+package classi;
+
+public class Area {
+	
+private	int idArea;
+private String Nome;
+private String idLav;
+private String idCant;
+
+
+public int getIdArea() {
+	return idArea;
+}
+public void setIdArea(int idArea) {
+	this.idArea = idArea;
+}
+public String getNome() {
+	return Nome;
+}
+public void setNome(String nome) {
+	Nome = nome;
+}
+public String getIdLav() {
+	return idLav;
+}
+public void setIdLav(String idLav) {
+	this.idLav = idLav;
+}
+public String getIdCant() {
+	return idCant;
+}
+public void setIdCant(String idCant) {
+	this.idCant = idCant;
+}
+
+
+
+}
